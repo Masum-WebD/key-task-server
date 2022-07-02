2a834d3c07e0f21577ace77c1681e9f61506e2f0
Md Masum Rana <masummx3@gmail.com>
2022-07-02 07:01:17 +0600

    task completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const port=process.env.PORT || 5000;
 const app = express();
 app.use(cors());
@@ -8,13 +9,14 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.User_Id}:${process.env.User_pass}@cluster0.bvpcziz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
     await client.connect();
     const AddTaskCollection =client.db("Add_Tasl").collection("Task")
+    const CompleteTaskCollection =client.db('C_Task').collection('Ctask')
     try{
         app.post('/task', async(req,res) =>{
             const addTask= req.body;
@@ -28,7 +30,15 @@ async function run(){
             const task=await cursor.toArray();
             res.send(task);
         })
+        app.get('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            const task= await AddTaskCollection.findOne(query);
+           res.send(task) 
+        });
         console.log('connect');
+
+
     }
     finally{
 
